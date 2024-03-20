ce0a17275c93ec063715b1896e2669542c5770d0
AI EMBED <31172146+aiembed@users.noreply.github.com>
2024-03-20 19:39:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     description='Intelligent route scanning engine',
     author='Dr. Sid Ryan',
     author_email='sid@aiembed.com',
-    py_modules=['drive_nav'],
+    py_modules=['drive_nav'],  # Correct module name
     entry_points={
         'console_scripts': [
-            'drive-nav=drive_nav:main'
+            'drive-nav=drive_nav:main'  # Correct function name
         ]
     },
     install_requires=[
