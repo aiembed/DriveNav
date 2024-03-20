bda7281260de8f14cab0eb0e51c0a2792016aa12
AI EMBED <31172146+aiembed@users.noreply.github.com>
2024-03-20 19:25:34 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='drive-nav',
+    version='1.0',
+    description='Intelligent route scanning engine',
+    author='Dr. Sid Ryan',
+    author_email='sid@aiembed.com',
+    py_modules=['drive-nav'],
+    entry_points={
+        'console_scripts': [
+            'drive-nav=drive-nav:main'
+        ]
+    },
+    install_requires=[
+        'picamera'
+    ],
+)
